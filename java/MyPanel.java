eb60fea8276c26f290756ea66f89c1bd3cc1597b
KValexander <ewoase@gmail.com>
2021-10-19 20:20:52 +0300

    player and gameloop upd

--- java/MyPanel.java
+++ java/MyPanel.java
@@ -9,9 +9,10 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 // Import own packages
+import com.main.entities.Player;
 import com.main.tiles.TileManager;
 
-public class MyPanel extends JPanel {
+public class MyPanel extends JPanel implements Runnable{
 
 	// Screen settings
 	private final int minTileSize = 16;
@@ -26,26 +27,24 @@ public class MyPanel extends JPanel {
 	private final int WIDTH = tileSize * maxScreenCol;
 	private final int HEIGHT = tileSize * maxScreenRow;
 
+	// FPS
+	private final int FPS = 30;
+
+	// Thread
+	Thread gameThread;
+
 	// Instances
 	KeyHandler keyH = new KeyHandler();
 	TileManager tileManager = new TileManager(this);
+	Player player = new Player(this, keyH);
 
 	// Constructor
-	MyPanel() {
+	public MyPanel() {
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
-	}
-
-	@Override
-	public void paintComponent(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-
-		drawGrid(g2d);
-		tileManager.draw(g2d);
-
-		g2d.dispose();
 	}
 
 	// Mesh rendering method
@@ -55,6 +54,65 @@ public class MyPanel extends JPanel {
 			g2d.drawLine(0, i, WIDTH, i);
 		for(int i = tileSize; i <= WIDTH; i += tileSize)
 			g2d.drawLine(i, 0, i, HEIGHT);
+	}
+
+	// Game thread
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	// Game loop
+	public void run() {
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+
+		while(gameThread != null) {
+			currentTime = System.nanoTime();
+
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+
+			if(delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}
+
+			if(timer >= 1000000000) {
+				System.out.println("FPS: " + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+
+		}
+	}
+
+	// Update
+	public void update() {
+
+		player.update();
+
+	}
+
+	// Paint component
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		Graphics2D g2d = (Graphics2D) g;
+
+		drawGrid(g2d);
+		player.draw(g2d);
+		tileManager.draw(g2d);
+
+		g2d.dispose();
 	}
 
 	// Get WIDTH
--- java/entities/Player.java
+++ java/entities/Player.java
@@ -1,5 +1,9 @@
 package com.main.entities;
 
+// Import packages
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 // Import own packages
 import com.main.MyPanel;
 import com.main.KeyHandler;
@@ -8,5 +12,31 @@ public class Player extends Entity{
 
 	MyPanel panel;
 	KeyHandler keyH;
+
+	// Constructor
+	public Player(MyPanel panel, KeyHandler keyH) {
+		this.panel = panel;
+		this.keyH = keyH;
+
+		setDefaultValues();
+	}
+
+	public void setDefaultValues() {
+		x = 100;
+		y = 100;
+		speed = 4;
+	}
+
+	public void update() {
+		if(keyH.upPressed == true) y -= speed;
+		if(keyH.downPressed == true) y += speed;
+		if(keyH.leftPressed == true) x -= speed;
+		if(keyH.rightPressed == true) x += speed;
+	}
+
+	public void draw(Graphics2D g2d) {
+		g2d.setColor(Color.white);
+		g2d.fillRect(x, y, panel.tileSize, panel.tileSize);
+	}
 
 }
