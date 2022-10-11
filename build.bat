add5ed4a8715152d55eb31daef93985b58076599
KValexander <ewoase@gmail.com>
2022-10-11 17:13:46 +0300

    bat upd

--- build.bat
+++ build.bat
@@ -2,12 +2,9 @@
 
 set src=src
 set bin=bin
+set class=com.main.Main
 
-if exist %src%/ goto start
-else goto end
-
-:end
-	echo "CODE FOLDER DOESN'T NOT EXISTS"
+if not exist %src%/ goto nf else goto start
 
 :start
 	powershell "Get-ChildItem -Path ./%src% -Name -File -s *.* > files.txt"
@@ -18,10 +15,24 @@ else goto end
 
 	rmdir /s /q %bin%
 	javac -d %bin% @source.txt
+	if not exist %bin%/ goto error
 
 	for /f "useback delims=" %%a IN ("files.txt") DO xcopy "%src%\%%a" "%bin%\%%a*"
-	del source.txt
-	del files.txt
+	del source.txt files.txt
 
 	cls
-	java -classpath ./%bin% com.main.Main
+	java -classpath ./%bin% %class%
+
+	goto end
+	
+:nf
+	echo "CODE FOLDER DOESN'T NOT EXISTS"
+	goto end
+
+:error
+	del source.txt files.txt
+	echo "COMPILATION ERROR"
+	goto end
+
+:end
+	pause
